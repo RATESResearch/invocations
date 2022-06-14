87734d24d884bfb82977aab9de6d21188375692b
Andrew Ernest <anernest@usa.net>
2022-06-13 20:07:15 -0500

    update autodoc.py

--- invocations/autodoc.py
+++ invocations/autodoc.py
@@ -68,7 +68,7 @@ class TaskDocumenter(
         # TODO: also, it may become moot-ish if we turn this all into emission
         # of custom domain objects and/or make the CLI arguments the focus
         # return autodoc.formatargspec(function, *getargspec(function))
-        return signature(function, *getargspec(function))
+        return signature(function)
 
     def document_members(self, all_members=False):
         # Neuter this so superclass bits don't introspect & spit out autodoc
